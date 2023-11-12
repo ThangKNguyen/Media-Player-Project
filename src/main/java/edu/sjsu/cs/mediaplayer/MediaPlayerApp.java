e01ed4b0a3150506ba4ebab3d71755271838509c
ThangAttack <thangsanjose@gmail.com>
2023-11-11 22:36:53 -0800

    testing, didn't change anyting

--- src/main/java/edu/sjsu/cs/mediaplayer/MediaPlayerApp.java
+++ src/main/java/edu/sjsu/cs/mediaplayer/MediaPlayerApp.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+//test comments
 public class MediaPlayerApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
